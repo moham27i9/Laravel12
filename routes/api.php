90da03c49de31bb47f45ead9f1cf97f48459bb39
unknown <rabieabo2@gmail.com>
2025-03-21 15:42:31 +0200

    addggroute

--- routes/api.php
+++ routes/api.php
@@ -9,4 +9,5 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', [AuthController::class, 'userProfile']);
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/gg', [AuthController::class, 'gg']);
 });
