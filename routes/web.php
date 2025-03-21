b35ddae803a98584f73d5a074d45ebaa8e56fe39
unknown <rabieabo2@gmail.com>
2025-03-21 16:20:01 +0200

    sss

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/', function () {
+    return view('welcome');
+});
 
